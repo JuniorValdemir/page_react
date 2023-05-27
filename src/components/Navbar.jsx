d4d76f82b7f0f6e1e8f73e006801501f91892613
JVNJ1 <juniorvaldemir87@gmail.com>
2023-05-27 18:43:35 -0300

    All project change

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -10,7 +10,7 @@ function Navbar() {
     }
 
     return (
-        <div className="flex justify-between items-center h-24 max-w-[1240px] mx-auto px-4 text-white">
+        <header className="flex justify-between items-center h-24 max-w-[1240px] mx-auto px-4 text-white">
             <h1 className="w-full text-3xl font-bold text-[#00ff7f]">REACT.</h1>
             <ul className="hidden md:flex">
                 <li className="p-4">Home</li>
@@ -32,7 +32,7 @@ function Navbar() {
                 <li className="p-4">Contact</li>
             </ul>
         </div>
-        </div>
+        </header>
     )
 }
 
